3bc7ce7b702844fe57ba8e0236a3b08396b5b479
TheBonBon <45827986+thebonbon@users.noreply.github.com>
2024-10-28 22:27:04 +0100

    Add attack / rotation speed GM attribute

--- src/Scripts/Game/Editor/Containers/Attributes/BON_AutoTurretEditorAttribute.c
+++ src/Scripts/Game/Editor/Containers/Attributes/BON_AutoTurretEditorAttribute.c
@@ -105,3 +105,53 @@ class BON_AutoTurretInaccuracyEditorAttribute : SCR_BaseValueListEditorAttribute
 			autoTurretComponent.m_fAttackInaccuracy = var.GetFloat();
 	}
 }
+
+[BaseContainerProps(), SCR_BaseEditorAttributeCustomTitle()]
+class BON_AutoTurretAttackSpeedEditorAttribute : SCR_BaseValueListEditorAttribute
+{
+	//------------------------------------------------------------------------------------------------
+	override SCR_BaseEditorAttributeVar ReadVariable(Managed item, SCR_AttributesManagerEditorComponent manager)
+	{
+		BON_AutoTurretComponent autoTurretComponent = BON_AutoTurretComponent.IsAutoTurret(item);
+		if (!autoTurretComponent)
+			return null;
+
+		return SCR_BaseEditorAttributeVar.CreateFloat(autoTurretComponent.m_fMaxAttackSpeed);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void WriteVariable(Managed item, SCR_BaseEditorAttributeVar var, SCR_AttributesManagerEditorComponent manager, int playerID)
+	{
+		if (!var)
+			return;
+
+		BON_AutoTurretComponent autoTurretComponent = BON_AutoTurretComponent.IsAutoTurret(item);
+		if (autoTurretComponent)
+			autoTurretComponent.m_fMaxAttackSpeed = var.GetFloat();
+	}
+}
+
+[BaseContainerProps(), SCR_BaseEditorAttributeCustomTitle()]
+class BON_AutoTurretRotationSpeedEditorAttribute : SCR_BaseValueListEditorAttribute
+{
+	//------------------------------------------------------------------------------------------------
+	override SCR_BaseEditorAttributeVar ReadVariable(Managed item, SCR_AttributesManagerEditorComponent manager)
+	{
+		BON_AutoTurretComponent autoTurretComponent = BON_AutoTurretComponent.IsAutoTurret(item);
+		if (!autoTurretComponent)
+			return null;
+
+		return SCR_BaseEditorAttributeVar.CreateFloat(autoTurretComponent.m_fRotationSpeed);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void WriteVariable(Managed item, SCR_BaseEditorAttributeVar var, SCR_AttributesManagerEditorComponent manager, int playerID)
+	{
+		if (!var)
+			return;
+
+		BON_AutoTurretComponent autoTurretComponent = BON_AutoTurretComponent.IsAutoTurret(item);
+		if (autoTurretComponent)
+			autoTurretComponent.m_fRotationSpeed = var.GetFloat();
+	}
+}
